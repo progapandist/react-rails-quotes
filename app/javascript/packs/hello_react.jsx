5eb41345d9c8223a90f1a53f1a16877f6d9869d9
Andy B <andybarnov@gmail.com>
2017-07-04 19:47:42 +0300

    display hello_react in pages#home

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="Andy" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
